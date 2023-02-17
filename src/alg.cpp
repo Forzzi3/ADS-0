9f181edea2613c02f852102d1b9eed0f8fa31f7a
Forzzi3 <125073237+Forzzi3@users.noreply.github.com>
2023-02-17 12:06:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a == 0 || b == 0)
+		return a + b;
+	if (a > b)
+		return nod(a - b, b);
+	else
+		return nod(a, b - a);
 }
