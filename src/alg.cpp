7466f5370fe6fb23bc2d49b099cb5bbc77f7f14c
Forzzi3 <125073237+Forzzi3@users.noreply.github.com>
2023-02-20 15:52:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,9 @@
 
 int gcd(int a, int b) {
     if (a == 0 || b == 0)
-		return a + b;
-	if (a > b)
-		return gcd(a - b, b);
-	else
-		return gcd(a, b - a);
+        return a + b;
+    if (a > b)
+        return gcd(a - b, b);
+    else
+        return gcd(a, b - a);
 }
